d4c8b61c06fc91d51b95da7210498d6bc67a46bc
providenceumugwaneza <providenceu43@gmail.com>
2025-11-17 20:42:01 +0200

    Initial Node.js Express setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// Current route
+app.get("/", (req, res) => {
+  res.send("Welcome to our new homepage!");
+});
+
+// New route
+app.get("/about", (req, res) => {
+  res.send("About Page: This is information about our web app.");
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
+
